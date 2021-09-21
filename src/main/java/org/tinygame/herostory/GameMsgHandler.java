6379690afb26ec86132cbe46a95af50bc8c142f2
hgq0916 <1286908118@qq.com>
2021-09-21 17:55:40 +0800

    创建netty服务器

---
+++ src/main/java/org/tinygame/herostory/GameMsgHandler.java
@@ -0,0 +1,32 @@
+package org.tinygame.herostory;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+
+/**
+ * @author hugangquan
+ * @date 2021/09/21 17:46
+ */
+public class GameMsgHandler extends SimpleChannelInboundHandler<Object> {
+
+
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("收到客户端消息:"+msg);
+
+        BinaryWebSocketFrame frame = (BinaryWebSocketFrame)msg;
+        ByteBuf content = frame.content();
+
+        byte[] bytes = new byte[content.readableBytes()];
+        content.readBytes(bytes);
+
+        System.out.println("收到的字节:");
+        for(byte b: bytes){
+            System.out.println(b);
+            System.out.print(",");
+        }
+        System.out.println();
+    }
+}
---
+++ src/main/java/org/tinygame/herostory/ServerMain.java
@@ -0,0 +1,56 @@
+package org.tinygame.herostory;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+/**
+ * @author hugangquan
+ * @date 2021/09/21 17:12
+ */
+public class ServerMain {
+
+    public static void main(String[] args) {
+        //两个线程池
+        //负责处理客户端连接,获取连接后把连接交给worker线程池处理
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        //负责读写客户端的消息
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup,workerGroup);
+
+        //绑定处理客户端连接类
+        bootstrap.channel(NioServerSocketChannel.class);
+        bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel sc) throws Exception {
+                sc.pipeline().addLast(new HttpServerCodec(),
+                        new HttpObjectAggregator(65535),
+                        new WebSocketServerProtocolHandler("/websocket"),
+                        new GameMsgHandler()
+                );
+            }
+        });
+
+        try{
+            ChannelFuture cf =  bootstrap.bind(12345).sync();
+
+            if(cf.isSuccess()){
+                System.out.println("服务器启动成功");
+            }
+            cf.channel().closeFuture().sync();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+}
