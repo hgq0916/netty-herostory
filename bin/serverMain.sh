36988239e44df1fd9bf3bb46255286bfc15f70c0
hgq0916 <1286908118@qq.com>
2021-11-30 22:40:02 +0800

    修改配置

---
+++ bin/serverMain.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#运行主类
+java_main_clazz=org.tinygame.herostory.ServerMain
+
+#//////////////////////////////////////////////////
+
+java_cmd="java -server -cp .:./lib/* -Xmx256m ${java_main_clazz}"
